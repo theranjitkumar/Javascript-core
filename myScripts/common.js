76e4225256809991731d192e3a6cddac9afb7b18
Ranjit Kumar <ranjit.kumar@MPSTECHNOLOGIES.COM>
2015-12-29 13:38:48 +0530

    a fuction added in namespace

--- myScripts/common.js
+++ myScripts/common.js
@@ -1,8 +1,11 @@
 
-var myApp =myApp || {}; // define if doesn't exist
-myApp.message = "Helo from myApp namespace message"
-$('#msg').text(myApp.message);
+// var myApp =myApp || {}; // define if doesn't exist
+// myApp.myName = function(name,age){
+// 	this.name = name;
+// 	this.age = age;
+// 	return this.name + "my age is " + this.age;
+// }
 
-var myApp = myApp || {}; // define if doesn't exist
-myApp.goodbymsg = "Good bye from myApp namespace";
-$('#msg').append('\n'+myApp.goodbymsg);
+// document.getElementById('msg2').innerHTML = myApp.myName('Ranjit kumar',30) ;
+
+
