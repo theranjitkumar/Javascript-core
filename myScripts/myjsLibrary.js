f415dbdedf62d81d59dfb7ac803aea760d640a6a
Ranjit Kumar <ranjit.kumar@MPSTECHNOLOGIES.COM>
2015-12-29 15:24:28 +0530

    z

--- myScripts/myjsLibrary.js
+++ myScripts/myjsLibrary.js
@@ -1,19 +1,12 @@
-(function () { 
-    console.log('I am inside an immediate function');
+(function(){
+console.log("i am clled imadiate function or self invoking function or self excuting function");
 }());
-
 
 (function () {    
 var returnedValue = (function (message) {
     return message;
 }('Hello Ranjit'));
 console.log(returnedValue); // will write hello in the console
-}());
-
-
-(function(){
-console.log("i am clled imadiate function or self invoking function or self excuting function");
-
 }());
 
 
