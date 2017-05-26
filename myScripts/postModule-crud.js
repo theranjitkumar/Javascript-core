ab539e2370257b60d4bdbf9c4d4ec5eb8426c9ae
Ranjit Kumar <web.ranjitkumar@gmail.com>
2017-05-26 21:59:23 +0530

    Create postModule-crud.js

---
+++ myScripts/postModule-crud.js
@@ -0,0 +1,62 @@
+
+var m = m || {};
+m.postsModule = function(){
+	this.name = "Posts Module";
+	this.moduleDetail = function(){
+		return this.name;
+	}
+	this.getData = function(){
+		var root = 'https://jsonplaceholder.typicode.com';
+		$.ajax({
+		  url: root + '/posts',
+		  method: 'GET'
+		}).then(function(data) {
+		  console.log(data);
+		});
+	}
+	this.getDataById = function(id){
+		var root = 'https://jsonplaceholder.typicode.com';
+		$.ajax({
+		  url: root + '/posts/'+id,
+		  method: 'GET'
+		}).then(function(data) {
+		  console.log(data);
+		});
+	}
+	this.postData = function(){
+		var root = 'https://jsonplaceholder.typicode.com';
+		$.ajax({
+		  url: root + '/posts',
+		  method: 'POST',
+		    data: {
+		    title: 'foo',
+		    body: 'bar',
+		    userId: 1
+		  }
+		}).then(function(data) {
+		  console.log(data);
+		});
+	}
+	this.putData = function(id){
+		$.ajax('http://jsonplaceholder.typicode.com/posts/'+id, {
+		  method: 'PUT',
+		  data: {
+		    id: 1,
+		    title: 'foo',
+		    body: 'bar',
+		    userId: 1
+		  }
+		}).then(function(data) {
+		  console.log(data);
+		});
+	}
+	this.deleteData = function(id){
+		$.ajax('http://jsonplaceholder.typicode.com/posts/'+id, {
+		  method: 'DELETE'
+		});
+	}
+
+}
+
+var postsObj = new m.postsModule();
+console.log(postsObj.moduleDetail())
