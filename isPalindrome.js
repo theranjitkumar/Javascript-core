0ebc31475fb8ec127d6c44562762596805372496
Ranjit Kumar <web.ranjitkumar@gmail.com>
2018-12-24 22:19:13 +0530

    Create isPalindrome.js

---
+++ isPalindrome.js
@@ -0,0 +1,10 @@
+var isPalindrome = function (string) {
+    if (string == string.split('').reverse().join('')) {
+        alert(string + ' is palindrome.');
+    }
+    else {
+        alert(string + ' is not palindrome.');
+    }
+}
+
+isPalindrome('12321');
